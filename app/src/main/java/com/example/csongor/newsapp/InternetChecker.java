5cfc9728e30e3f91fe58f68bec59c18226d0e274
Horváth Csongor <horvath.csongor.3d@gmail.com>
2018-06-03 19:34:34 +0200

    add InternetChecker

---
+++ app/src/main/java/com/example/csongor/newsapp/InternetChecker.java
@@ -0,0 +1,45 @@
+package com.example.csongor.newsapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Class for checking internet connection status.
+ * This class has a listener for connection status callback
+ * Idea and suggestion from previous reviewer, documented at
+ * https://www.androidhive.info/2012/07/android-detect-internet-connection-status/
+ */
+public class InternetChecker extends BroadcastReceiver {
+
+    public static InternetCheckListener mInternetCheckListener;
+
+    public InternetChecker() {
+        super();
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+        if (mInternetCheckListener != null) {
+            mInternetCheckListener.onInternetStatusChanged(isConnected);
+        }
+    }
+
+    public static boolean isConnectedToNet () {
+        ConnectivityManager connectivityManager = (ConnectivityManager) NewsApp.getInstance()
+                .getApplicationContext()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    }
+
+    public interface InternetCheckListener {
+        void onInternetStatusChanged(boolean isConnected);
+    }
+}
